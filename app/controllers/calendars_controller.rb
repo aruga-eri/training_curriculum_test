ff440b2b5ab4f0562896134f490da2d0ab863ec3
aruga-eri <eri0725.t@gmail.com>
2024-08-12 14:56:23 +0900

    Issue4~6再修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,16 +8,11 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    @plan = Plan.new(plan_params)
-    if @plan.save
-      redirect_to action: :index
-    else
-      render :new
-    end
+    Plan.create(plan_params)
+    redirect_to action: :index
   end
 
   private
-
   def plan_params
     params.require(:plan).permit(:date, :plan)
   end
@@ -30,7 +25,6 @@ class CalendarsController < ApplicationController
     # 例)　今日が2月1日の場合・・・ Date.today.day => 1日
 
     @week_days = []
-
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
@@ -44,7 +38,7 @@ class CalendarsController < ApplicationController
         wday_num = wday_num -7
       end
 
-      days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans, :wday => wdays[wday_num] }
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wday: wdays[wday_num] }
       @week_days.push(days)
     end
 
